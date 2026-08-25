b9abd6ac00053fd1506c0b875d55e8f3cb90dcb2
SupportingBasesOfficial <suportingbases@gmail.com>
2026-08-25 14:14:20 -0300

    test: pin latest Wave 1 panoramic substitutions

--- tests/wave1/test_manifest.py
+++ tests/wave1/test_manifest.py
@@ -23,6 +23,10 @@ REQUIRED_PANORAMIC_SUBSTITUTIONS = {
     "resource_scope_absence_for_resource_authority",
     "resource_scope_on_non_resource_authority",
     "table_acl_clean_for_column_acl_clean",
+    "object_acl_clean_for_predefined_all_data_role_absent",
+    "table_shape_match_for_referential_action_free",
+    "primary_key_present_for_immediate_valid_ready_conflict_arbiter",
+    "cross_tenant_action_match_for_target_set_authority",
 }
 
 

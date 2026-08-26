8b3f8894936a678e97203eefe92cbebd44bfe889
SupportingBasesOfficial <suportingbases@gmail.com>
2026-08-26 10:00:27 -0300

    impl(wave3): expose release authority chain primitives

--- src/jlmirror_release/__init__.py
+++ src/jlmirror_release/__init__.py
@@ -1,41 +1,20 @@
 """Wave 3 vendor-neutral release and deployment authority primitives."""
 
-from .authority import (
-    DeploymentAuthority,
-    DeploymentObservation,
-    DeploymentRecord,
-    ReleaseTargetState,
-    require_trusted_build_source,
-)
+from .authority import DeploymentAuthority, DeploymentObservation, DeploymentRecord, ReleaseTargetState, require_trusted_build_source
+from .compatibility import CellCompatibilityEvidence, MixedVersionMatrix, RolloutCompatibilityEvidence, require_rollout_compatibility
 from .configuration import ConfigurationValidationEvidence, require_validation_for_target
-from .model import (
-    ArtifactIdentity,
-    DeploymentIntent,
-    OutcomeClass,
-    PromotionState,
-    ReleaseError,
-    SourceTrustClass,
-    TargetConfiguration,
-    ValidationScope,
-)
+from .model import ArtifactIdentity, DeploymentIntent, OutcomeClass, PromotionState, ReleaseError, SourceTrustClass, TargetConfiguration, ValidationScope
+from .provenance import BuildProvenanceEvidence, PromotionEvidence, require_promotion_authority, validate_build_provenance
+from .recovery import RecoveryClassificationEvidence, classify_change_outcome
 from .verification import RuntimeVerificationEvidence, verify_runtime
 
 __all__ = [
-    "ArtifactIdentity",
-    "ConfigurationValidationEvidence",
-    "DeploymentAuthority",
-    "DeploymentIntent",
-    "DeploymentObservation",
-    "DeploymentRecord",
-    "OutcomeClass",
-    "PromotionState",
-    "ReleaseError",
-    "ReleaseTargetState",
-    "RuntimeVerificationEvidence",
-    "SourceTrustClass",
-    "TargetConfiguration",
-    "ValidationScope",
-    "require_trusted_build_source",
-    "require_validation_for_target",
-    "verify_runtime",
+    "ArtifactIdentity", "BuildProvenanceEvidence", "CellCompatibilityEvidence",
+    "ConfigurationValidationEvidence", "DeploymentAuthority", "DeploymentIntent",
+    "DeploymentObservation", "DeploymentRecord", "MixedVersionMatrix", "OutcomeClass",
+    "PromotionEvidence", "PromotionState", "RecoveryClassificationEvidence", "ReleaseError",
+    "ReleaseTargetState", "RolloutCompatibilityEvidence", "RuntimeVerificationEvidence",
+    "SourceTrustClass", "TargetConfiguration", "ValidationScope", "classify_change_outcome",
+    "require_promotion_authority", "require_rollout_compatibility", "require_trusted_build_source",
+    "require_validation_for_target", "validate_build_provenance", "verify_runtime",
 ]
